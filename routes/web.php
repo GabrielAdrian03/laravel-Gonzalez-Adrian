f19e2c50c7536ac1cd1bea43197fbd9845c2d68f
GabrielAdrian03 <gabrieladrianxh@gmail.com>
2025-06-16 23:00:46 -0300

    Arreglos a las tareas

--- routes/web.php
+++ routes/web.php
@@ -2,53 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TareaController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', [TareaController::class, 'index']);
-Route::post('/tareas', [TareaController::class, 'store']);
-Route::get('/tareas/{id}/edit', [TareaController::class, 'edit']);
-Route::put('/tareas/{id}', [TareaController::class, 'update']);
-Route::delete('/tareas/{id}', [TareaController::class, 'destroy']);
-
-
+// Ruta principal
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Autenticación
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Home
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-
-Route::get('/tareas', [App\Http\Controllers\TareaController::class, 'index'])
-    ->name('tareas.index');
-
-
-Route::get('/tareas/create', [App\Http\Controllers\TareaController::class, 'create'])
-    ->name('tareas.create');
-
-
-Route::post('/tareas', [App\Http\Controllers\TareaController::class, 'store'])
-    ->name('tareas.store');
-
-
-Route::get('/tareas/{tarea}/edit', [App\Http\Controllers\TareaController::class, 'edit'])
-    ->name('tareas.edit');
-
-
-Route::put('/tareas/{tarea}', [App\Http\Controllers\TareaController::class, 'update'])
-    ->name('tareas.update');
-
-
-Route::delete('/tareas/{tarea}', [App\Http\Controllers\TareaController::class, 'destroy'])
-    ->name('tareas.destroy');
+// Tareas
+Route::get('/tareas', [TareaController::class, 'index'])->name('tareas.index');
+Route::get('/tareas/create', [TareaController::class, 'create'])->name('tareas.create');
+Route::post('/tareas', [TareaController::class, 'store'])->name('tareas.store');
+Route::get('/tareas/{tarea}/edit', [TareaController::class, 'edit'])->name('tareas.edit');
+Route::put('/tareas/{tarea}', [TareaController::class, 'update'])->name('tareas.update');
+Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])->name('tareas.destroy');
